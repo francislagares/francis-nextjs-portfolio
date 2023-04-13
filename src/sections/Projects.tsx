757c2cd2ef7e0346afb7909bf9617083f90fab60
Francis Lagares <francis.lagares@gmail.com>
2023-04-13 18:35:53 +0200

    style: center content into viewport for mobile screens

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -8,7 +8,7 @@ const Projects = () => {
   const projectsData = [
     {
       image: "/krypt.png",
-      projectName: "Krypt - Web 3.0 Blockchain Application",
+      projectName: "Krypt - Web 3.0 Application",
       projectDescription:
         "Krypt is a Web3.0 blockchain application that enables users to send transactions over the blockchain, with each transaction being permanently recorded on the blockchain.",
       projectTech: [
