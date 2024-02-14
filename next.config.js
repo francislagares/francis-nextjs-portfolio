eaf9cb3e089e4c4fc929c95f6fdc3591e3d2dc0a
Francis Lagares <francis.lagares@gmail.com>
2024-02-14 14:25:28 +0100

    feat: create GoogleAnalytics component

--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  async headers() {
+    return [
+      {
+        source: '/_next/:path*',
+        headers: [
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: '*',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,8 @@ import '@/scss/index.scss';
 import type { AppProps } from 'next/app';
 import { Fira_Code, Raleway } from 'next/font/google';
 
+import GoogleAnalytics from '@/components/GoogleAnalytics';
+
 const raleway = Raleway({ subsets: ['latin'] });
 const firaCode = Fira_Code({
   weight: ['300', '400', '500', '600', '700'],
@@ -13,6 +15,9 @@ const firaCode = Fira_Code({
 const App = ({ Component, pageProps }: AppProps) => {
   return (
     <>
+      {process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS && (
+        <GoogleAnalytics ga_id={process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS} />
+      )}
       <style jsx global>{`
         :root {
           --raleway: ${raleway.style.fontFamily};
